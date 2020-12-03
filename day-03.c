eda27b9bfffd35e59dc6930a4820230612001051
luk <mail-login+gitlab@protonmail.com>
2020-12-03 11:16:21 +0100

    Day03

---
+++ day-03.c
@@ -0,0 +1,61 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "./read_file.h"
+
+int part1(struct string_content* input, int change_x, int change_y, int print){
+	int x = 0;
+	int y = 0; // amount of lines is the height of the field
+
+	int x_max = strlen(input->content[0]);
+
+	int counter_trees = 0;
+	int counter_empty = 0;
+	while(y < input->length){
+		if (print){
+			printf("x:%d y:%d -> %c\t", x, y, input->content[y][x]);
+		}
+		if(input->content[y][x] == '#'){
+			if (print){
+				printf("Tree\n");
+			}
+			counter_trees++;
+		} else {
+			if (print){
+				printf("no Tree\n");
+			}
+			counter_empty++;
+		}
+		y += change_y;
+		x = (x + change_x) % x_max;
+	}
+	return counter_trees;
+}
+
+int part2(struct string_content* input){
+	int ret = 1;
+	ret *= part1(input, 1, 1, 0);
+	ret *= part1(input, 3, 1, 0);
+	ret *= part1(input, 5, 1, 0);
+	ret *= part1(input, 7, 1, 0);
+	ret *= part1(input, 1, 2, 0);
+
+	return ret;
+}
+
+int main() {
+	char* type;
+	(void)type;
+
+	struct string_content* input = read_file("./day-03.dat", type);
+	printf("Part1\nTrees: %d\n", part1(input, 3, 1, 0));
+	free_file(input);
+
+	input = read_file("./day-03.dat", type);
+	printf("Part2\nTrees: %d\n", part2(input));
+	free_file(input);
+
+	return EXIT_SUCCESS;
+}
