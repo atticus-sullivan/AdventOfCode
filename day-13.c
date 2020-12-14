8de2d7ef26f896420cd52e236cb5d65c02875609
luk <mail-login+gitlab@protonmail.com>
2020-12-14 11:41:49 +0100

    solved day-13 with help

--- day-13.c
+++ day-13.c
@@ -6,6 +6,10 @@
 
 #include "./read_file.h"
 
+int compare(const void * a, const void * b){
+	return *(int*)b - *(int*)a;
+}
+
 // TODO inline
 int kl_v(int a, int b, int ggt){
 	return (a*b)/ggt;
@@ -51,6 +55,9 @@ void eea_start(int a, int b, int ret[2]){
 		eea(a,b, ret);
 	} else {
 		eea(b,a, ret);
+		int tmp = ret[1];
+		ret[1] = ret[0];
+		ret[0] = tmp;
 	}
 }
 
@@ -96,6 +103,7 @@ int part2(struct string_content* input, int amount){
 	}
 
 	int last = arr[0];
+	int l_offset = 0;
 	for(int i=1; i < amount; i++){
 		printf("Processing %d\n", arr[i]);
 		int ggt = ea_start(last,arr[i]);
@@ -105,12 +113,65 @@ int part2(struct string_content* input, int amount){
 
 		printf("ggt: %d  eea: %d %d   kgv: %d\n", ggt, alpha_beta[0], alpha_beta[1], kgv);
 
-		int hit = (((-offset[i]/ggt)*alpha_beta[0]*last) % kgv + kgv) % kgv;
+		int hit;
+		if(last < arr[i]){
+			hit = (((l_offset-offset[i]/ggt)*alpha_beta[0]*last - l_offset) % kgv + kgv) % kgv;
+		} else {
+			hit = (((l_offset-offset[i]/ggt)*alpha_beta[0]*last - l_offset) % kgv + kgv) % kgv;
+		}
 		printf("%-3d and %-3d hit at %-5d with offset %d\n\n", arr[i], last, hit, offset[i]);
 
-		last = hit;
+		/* l_offset = hit; */
+		last = kgv;
 	}
 	return last;
+}
+
+int search(int a, int* b){
+	int i = 0;
+	while(b[i] != a) i++;
+	return i;
+}
+
+long part2b(struct string_content* input, int amount){
+	int* arr    = malloc(sizeof(int)*amount);
+	int* arr_s  = malloc(sizeof(int)*amount);
+	int* offset = malloc(sizeof(int)*amount);
+	int* mods   = malloc(sizeof(int)*amount);
+	char* num = strtok(input->content[1], ",");
+	int i = 0;
+	for(int j = 0; num != NULL && i < amount; j++){
+		if(*num != 'x'){
+			offset[i] = j;
+			arr[i]    = atoi(num);
+			arr_s[i]  = arr[i];
+			mods[i]   = (-j % arr[i] + arr[i]) % arr[i];
+			i++;
+		}
+		num = strtok(NULL, ",");
+	}
+
+	printf("%-5s\t%-5s\t%-5s\n", "offset", "mod", "line");
+	for(int i=0; i < amount; i++){
+		printf("%-5d\t", offset[i]);
+		printf("%-5d\t", mods[i]);
+		printf("%-5d\n", arr[i]);
+	}
+	qsort(arr_s, amount, sizeof(int), compare);
+	long r = arr_s[0];
+	long v = mods[search(arr_s[0], arr)];
+	printf("r: %ld\t v: %ld\n", r, v);
+
+	for(int i=1; i < amount; i++){
+		long b = arr_s[i];
+		printf("b: %ld\n", b);
+		printf("mods[b]: %d (search for b in arr)\n", mods[search(b, arr)]);
+		while(v % b != mods[search(b, arr)]){
+			v += r;
+		}
+		r *= b;
+	}
+	return v;
 }
 
 int main() {
@@ -130,8 +191,8 @@ int main() {
 	printf("Part1:\n%d\n", part1(input));
 	free_file(input);
 	
-	input = read_file("./day-13.dat.testing2", type);
-	printf("\nPart2:\n%d\n", part2(input,5));
+	input = read_file("./day-13.dat", type);
+	printf("\nPart2:\n%ld\n", part2b(input,9));
 	free_file(input);
 	return EXIT_SUCCESS;
 }
