d6d02ecb24dead1df1cbb7bbe1706171ead8613a
luk <mail-login+gitlab@protonmail.com>
2021-12-02 13:21:31 +0100

    solved 02

---
+++ 2021_lua/day02.lua
@@ -0,0 +1,43 @@
+local part1 = {}
+function part1.forward(pos, x) pos.hor   = pos.hor   + x end
+function part1.up(pos, x)      pos.depth = pos.depth - x end
+function part1.down(pos, x)    part1.up(pos, -x) end
+
+local part2 = {}
+function part2.forward(pos, x)
+	pos.hor = pos.hor + x
+	pos.depth = pos.depth + pos.aim*x
+end
+function part2.up(pos, x)
+	pos.aim = pos.aim - x
+end
+function part2.down(pos, x)
+	part2.up(pos, -x)
+end
+
+
+local function part(file, funcSet)
+	local pos = {hor=0, depth=0, aim=0}
+	local n
+	for line in file:lines() do
+		n = line:match("%d+")
+		if line:match("^forward")  then funcSet.forward(pos, n)
+		elseif line:match("^down") then funcSet.down(pos, n)
+		elseif line:match("^up")   then funcSet.up(pos, n)
+		else
+			error("Failed parsing line ", line)
+		end
+		-- print(line)
+		-- print("hor", pos.hor, "depth", pos.depth, "aim", pos.aim, "\n")
+	end
+	return pos.hor * pos.depth
+end
+
+local file
+file = io.open("./day02.dat")
+print("Part1:", part(file, part1))
+file:close()
+
+file = io.open("./day02.dat")
+print("Part2:", part(file, part2))
+file:close()
