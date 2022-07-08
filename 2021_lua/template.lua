2d72aaefa11e2bac981a40c838a0d64efe04167b
luk <mail-login+gitlab@protonmail.com>
2022-07-08 22:16:22 +0200

    added lua template

---
+++ 2021_lua/template.lua
@@ -0,0 +1,19 @@
+local _M = {}
+
+local function parse_file(file)
+	for line in file:lines() do
+		print(line)
+	end
+end
+
+function _M.part1(file)
+	parse_file(file)
+	print(file)
+end
+
+function _M.part2(file)
+	parse_file(file)
+	print(file)
+end
+
+return _M
