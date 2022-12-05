5f04bca0c675aaf89cd1cf36b67d1143ee595e47
Lukas <mail-login+gitlab@protonmail.com>
2022-12-05 23:51:53 +0100

    add a template for future days

---
+++ 2022_Cpp/template.cpp
@@ -0,0 +1,33 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cassert>
+
+#include "aocutils.h"
+
+struct Class {
+	int amount;
+	int src, dst;
+
+	friend std::istream& operator>>(std::istream& is, Class& i) {
+		i = std::move(Class{}); // default initialize the most probable moved from object
+		return is;
+	}
+};
+
+int main(int argc, char* argv[]) {
+	// std::ifstream ifs{"../day00.dat.testing"};
+	std::ifstream ifs{"../day00.dat"};
+	if(ifs.fail()) throw std::runtime_error("File couldn't be opened!");
+
+	std::vector<Class> instructions = aocutils::vectorize_ifs<Class>(ifs);
+
+	std::string part1 = "dummy";
+	std::string part2 = "dummy";
+
+	std::cout << "Day 04:" << '\n'
+		<< "  Part 1: " << part1 << std::endl
+		<< "  Part 2: " << part2 << std::endl
+		;
+}
