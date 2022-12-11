6bd0691f853436d1f46e7748d72b9058dcd8c4d6
Lukas <mail-login+gitlab@protonmail.com>
2022-12-11 22:18:09 +0100

    Clanup day11 and format it

--- 2022_Cpp/day11.cpp
+++ 2022_Cpp/day11.cpp
@@ -1,34 +1,34 @@
 #include <cassert>
+#include <deque>
 #include <fstream>
 #include <functional>
 #include <iostream>
-#include <map>
-#include <vector>
-#include <deque>
 #include <limits>
-#include <sstream>
 #include <numeric>
-#include <ranges>
+#include <sstream>
+#include <vector>
 
 #include "aocutils.h"
 
 constexpr auto max_size = std::numeric_limits<std::streamsize>::max();
 
-bool is_number(const std::string &s) {
-  return !s.empty() && std::all_of(s.begin(), s.end(), ::isdigit);
+bool is_number(const std::string &s)
+{
+	return !s.empty() && std::all_of(s.begin(), s.end(), ::isdigit);
 }
 
-std::function<long(long)> func_factory(std::string op1, std::string op, std::string op2)
+std::function<long(long)> func_factory(std::string op1, std::string op,
+									   std::string op2)
 {
-	std::function<long(long,long)> opl{};
-	char o = op.at(0);
+	std::function<long(long, long)> opl{};
+	char                            o = op.at(0);
 	switch(o)
 	{
 		case '*':
-			opl = [](long op1, long op2){return op1*op2;};
+			opl = [](long op1, long op2) { return op1 * op2; };
 			break;
 		case '+':
-			opl = [](long op1, long op2){return op1+op2;};
+			opl = [](long op1, long op2) { return op1 + op2; };
 			break;
 		default:
 			throw std::runtime_error{"Invalid operation"};
@@ -37,37 +37,39 @@ std::function<long(long)> func_factory(std::string op1, std::string op, std::str
 	std::function<long(long)> ret{};
 	if(op1 == "old" && op2 == "old")
 	{
-		ret = [opl](long old) {return opl(old,old);};
+		ret = [opl](long old) { return opl(old, old); };
 	}
 	else if(op1 == "old")
 	{
 		assert(is_number(op2));
 		long op2i{std::stol(op2)};
-		ret = [opl,op2i](long old) {return opl(old,op2i);};
-	} else
+		ret = [opl, op2i](long old) { return opl(old, op2i); };
+	}
+	else
 	{
 		assert(is_number(op1));
 		assert(is_number(op2));
 		long op1i{std::stol(op2)};
 		long op2i{std::stol(op2)};
-		ret = [opl,op1i,op2i](long _) {return opl(op1i,op2i);};
+		ret = [opl, op1i, op2i](long) { return opl(op1i, op2i); };
 	}
 	return ret;
 }
 
 struct Monkey
 {
-	std::deque<long> items;
+	std::deque<long>          items;
 	std::function<long(long)> op;
-	int div_able;
-	unsigned int throw_true;
-	unsigned int throw_false;
-	unsigned long inspected;
+	int                       div_able;
+	unsigned int              throw_true;
+	unsigned int              throw_false;
+	unsigned long             inspected;
 
-	void exec(std::vector<Monkey>& monkeys, std::function<long(long)> red)
+	void exec(std::vector<Monkey> &monkeys, std::function<long(long)> red)
 	{
 		long item{};
-		while(!items.empty()){
+		while(!items.empty())
+		{
 
 			item = items.front();
 			items.pop_front();
@@ -81,10 +83,8 @@ struct Monkey
 			if(item % div_able == 0)
 			{
 				monkeys.at(throw_true).items.push_back(item);
-			} else
-			{
-				monkeys.at(throw_false).items.push_back(item);
 			}
+			else { monkeys.at(throw_false).items.push_back(item); }
 		}
 	}
 
@@ -101,7 +101,7 @@ struct Monkey
 		if(!is.good()) return is;
 		std::getline(is, l);
 		std::istringstream ls{l};
-		int j{};
+		int                j{};
 		while(ls.good())
 		{
 			ls >> j;
@@ -159,96 +159,38 @@ struct Monkey
 		assert(l == "monkey");
 		is >> i.throw_false;
 
-		std::getline(is, l);
-		std::getline(is, l); // TODO
+		std::getline(is, l); // finish line
+		std::getline(is, l); // skip the blank line in between
+		if(is.eof())
+			is.clear(); // set good bit so that the last monky is inserted as
+						// well
 
 		return is;
 	}
 	friend std::ostream &operator<<(std::ostream &os, const Monkey &i)
 	{
-		for(const auto& ele : i.items)
-		{
-			os << ele << " ";
-		}
+		for(const auto &ele : i.items) { os << ele << " "; }
 		return os;
 	}
 };
 
-long lA(long a){
-	return a/3;
-}
-unsigned long partA(std::vector<Monkey> monkeys)
+template <unsigned rounds>
+unsigned long part(std::vector<Monkey> monkeys, std::function<long(long)> red)
 {
-	// std::cout << "\n";
-	// std::cout << monkeys.size() << std::endl;
-	// for(const auto& m : monkeys){
-	// 	std::cout << m;
-	// }
-	// std::cout << "\n";
 
-
-	for(int i{0}; i < 20; i++){
-		for(auto& m : monkeys){
-			m.exec(monkeys, lA);
-		}
-		// std::cout << "\n";
-		// std::cout << monkeys.size() << std::endl;
-		// for(const auto& m : monkeys){
-		// 	std::cout << m << "(" << m.inspected << ")" << std::endl;
-		// }
+	for(unsigned int i{0}; i < rounds; i++)
+	{
+		for(auto &m : monkeys) { m.exec(monkeys, red); }
 	}
 
 	std::array<Monkey, 2> top{};
-	std::partial_sort_copy(
-			monkeys.begin(), monkeys.end(),
-			top.begin(), top.end(),
-			[](const Monkey& a, const Monkey& b){return a.inspected > b.inspected;}
-			);
+	std::partial_sort_copy(monkeys.begin(), monkeys.end(), top.begin(),
+						   top.end(),
+						   [](const Monkey &a, const Monkey &b)
+						   { return a.inspected > b.inspected; });
 
-
-	// std::cout << top[0].inspected << top[1].inspected << std::endl;
-	auto part1 = top[0].inspected * top[1].inspected;
-	return part1;
-}
-
-unsigned long partB(std::vector<Monkey> monkeys)
-{
-	// std::cout << "\n";
-	// std::cout << monkeys.size() << std::endl;
-	// for(const auto& m : monkeys){
-	// 	std::cout << m;
-	// }
-	// std::cout << "\n";
-
-	auto lcm = monkeys | std::views::transform([](const Monkey& a){return a.div_able;});
-	const int ring = std::reduce(lcm.begin(), lcm.end(), 1, [](const int a, const int b){return std::lcm(a, b);});
-	// std::cout << "Ring: " << ring << std::endl;
-
-	for(int i{0}; i < 10000; i++){
-		for(auto& m : monkeys){
-			m.exec(monkeys, [ring](long a){return a%ring;});
-		}
-		// if(i+1 == 1 || i+1 == 20 || (i+1) % 1000 == 0)
-		// {
-		// 	std::cout << "\n";
-		// 	std::cout << "Round: " << i+1 << std::endl;
-		// 	for(const auto& m : monkeys){
-		// 		std::cout << m << "(" << m.inspected << ")" << std::endl;
-		// 	}
-		// }
-	}
-
-	std::array<Monkey, 2> top{};
-	std::partial_sort_copy(
-			monkeys.begin(), monkeys.end(),
-			top.begin(), top.end(),
-			[](const Monkey& a, const Monkey& b){return a.inspected > b.inspected;}
-			);
-
-
-	// std::cout << top[0].inspected << " " << top[1].inspected << std::endl;
-	auto part1 = top[0].inspected * top[1].inspected;
-	return part1;
+	auto ret = top[0].inspected * top[1].inspected;
+	return ret;
 }
 
 int main()
@@ -258,12 +200,17 @@ int main()
 	if(ifs.fail()) throw std::runtime_error("File couldn't be opened!");
 
 	std::vector<Monkey> monkeys = aocutils::vectorize_ifs<Monkey>(ifs);
+	std::cout << monkeys.size() << std::endl;
+	// should be able to parallelize this somehow
+	long lcm_div_able = std::accumulate(monkeys.begin(), monkeys.end(), 1,
+										[](const int a, const Monkey b)
+										{ return std::lcm(a, b.div_able); });
 
-	auto part1 = partA(monkeys);
-	auto part2 = partB(monkeys);
+	auto part1 = part<20>(monkeys, [](long a) { return a / 3; });
+	auto part2 = part<10000>(monkeys, [lcm_div_able](long a)
+							 { return a % lcm_div_able; });
 
 	std::cout << "Day 11:" << '\n'
 			  << "  Part 1: " << part1 << std::endl
-			  << "  Part 2: " << part2 << std::endl
-			  ;
+			  << "  Part 2: " << part2 << std::endl;
 }
