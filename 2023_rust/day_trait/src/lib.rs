ae9212810a4800458d5c76834b13d46720381c65
Lukas <mail-login+gitlab@protonmail.com>
2023-12-03 20:55:06 +0100

    2023 setup for rust

---
+++ 2023_rust/day_trait/src/lib.rs
@@ -0,0 +1,7 @@
+use anyhow::Result;
+
+pub trait Day {
+    fn parse(&mut self) -> Result<()>;
+    fn solve_part1(&mut self) -> Result<()>;
+    fn solve_part2(&mut self) -> Result<()>;
+}
---
+++ 2023_rust/main/src/main.rs
@@ -0,0 +1,72 @@
+use clap::Parser;
+use day_trait::Day;
+use anyhow::{Context, Result, anyhow};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Day which to run. Defaults to all days.
+    #[arg(short, long)]
+    day: Option<usize>,
+
+    /// Part which to run. Defaults to both parts.
+    #[arg(short, long)]
+    part: Option<usize>,
+}
+
+fn main() -> Result<()> {
+    let mut days: [Box<dyn Day>; 0] = [
+    ];
+
+    let args = Cli::parse();
+
+    match args.day {
+        Some(d) => {
+            let x = days.get_mut(d-1).context("invalid day provided")?;
+            x.parse()?;
+            match args.part {
+                Some(1) => {
+                    x.solve_part1()?;
+                },
+                Some(2) => {
+                    x.solve_part2()?;
+                },
+                Some(_) => {
+                    return Err(anyhow!("invalid part provided"));
+                }
+                None => {
+                    x.solve_part1()?;
+                    x.solve_part2()?;
+                },
+            }
+        },
+        None => {
+            let mut first = true;
+            for x in days.iter_mut() {
+                if !first {
+                    println!("");
+                } else {
+                    first = false;
+                }
+                x.parse()?;
+                match args.part {
+                    Some(1) => {
+                        x.solve_part1()?;
+                    },
+                    Some(2) => {
+                        x.solve_part2()?;
+                    },
+                    Some(_) => {
+                        return Err(anyhow!("invalid part provided"));
+                    }
+                    None => {
+                        x.solve_part1()?;
+                        x.solve_part2()?;
+                    },
+                }
+            }
+        },
+    }
+
+    return Ok(());
+}
