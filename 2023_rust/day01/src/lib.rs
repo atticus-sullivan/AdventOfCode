19c812afb3007e25040267971e31a119958c5caf
Lukas <mail-login+gitlab@protonmail.com>
2023-12-04 18:25:55 +0100

    add stub for benchmarking day01 and implement a different approach than replacing (thanks @Tobias Lasser)

--- 2023_rust/day01/src/lib.rs
+++ 2023_rust/day01/src/lib.rs
@@ -2,7 +2,11 @@ use day_trait::Day;
 use std::fs::File;
 use std::io::{self, BufRead};
 use anyhow::{Context, Result};
+// use regex::Regex;
 
+const DIGITS:[&str;10] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+#[derive(Clone,Debug)]
 pub struct Day01 {
     lines: Vec<String>,
 }
@@ -13,11 +17,58 @@ impl Day01 {
             lines: vec![],
         }
     }
-}
 
-impl Day for Day01 {
-    fn parse(&mut self) -> Result<()> {
-        let file = File::open("./problems/day01.dat").context("reading input file failed")?;
+    pub fn solve_part2_replace(&mut self) -> Result<String> {
+        let mut ret = 0;
+        for l in &mut self.lines {
+
+            // let l_old = l.clone();
+            replace(l)?;
+
+            let mut first:i32 = -1;
+            let mut last  = 0;
+            for c in l.chars() {
+                if c.is_digit(10) {
+                    if first == -1 {
+                        first = c.to_digit(10).context("char to int failed")? as i32;
+                    }
+                    last = c.to_digit(10).context("char to int failed")?;
+                }
+            }
+            // println!("{l_old} {}", (first as u32)*10 + last);
+            ret += (first as u32)*10 + last;
+        }
+
+        Ok(ret.to_string())
+    }
+
+    pub fn solve_part2_collect(&mut self) -> Result<String> {
+        let mut ret = 0;
+
+        for l in &self.lines {
+            let (first,last) = collect(l);
+            // println!("{l} -> {}", first*10 + last);
+            ret += first*10 + last;
+        }
+
+        Ok(ret.to_string())
+    }
+
+    // pub fn solve_part2_regex(&mut self) -> Result<String> {
+    //     let mut ret = 0;
+    //
+    //     for l in &self.lines {
+    //         let (first,last) = regex(l);
+    //         // println!("{l} -> {}", first*10 + last);
+    //         ret += first*10 + last;
+    //     }
+    //
+    //     Ok(ret.to_string())
+    // }
+
+    // needed for benchmarking
+    pub fn parse_with_prefix(&mut self, prefix: &str) -> Result<()> {
+        let file = File::open(prefix.to_string() + "problems/day01.dat").context("reading input file failed")?;
         // let file = File::open("./problems/day01.dat.testing").context("reading input file failed")?;
 
         let buf = io::BufReader::new(file);
@@ -25,6 +76,12 @@ impl Day for Day01 {
             self.lines.push(line?);
         }
         Ok(())
+    }
+}
+
+impl Day for Day01 {
+    fn parse(&mut self) -> Result<()> {
+        Ok(self.parse_with_prefix("./")?)
     }
 
     fn solve_part1(&mut self) -> Result<String> {
@@ -47,30 +104,86 @@ impl Day for Day01 {
     }
 
     fn solve_part2(&mut self) -> Result<String> {
-        let mut ret = 0;
-        for l in &mut self.lines {
-
-            replace(l)?;
-
-            let mut first:i32 = -1;
-            let mut last  = 0;
-            for c in l.chars() {
-                if c.is_digit(10) {
-                    if first == -1 {
-                        first = c.to_digit(10).context("char to int failed")? as i32;
-                    }
-                    last = c.to_digit(10).context("char to int failed")?;
-                }
-            }
-            ret += (first as u32)*10 + last;
-        }
-
-        Ok(ret.to_string())
+        // Ok(self.solve_part2_replace()?)
+        Ok(self.solve_part2_collect()?)
     }
 }
 
+// FIXME: This doesn't work because no rfind for regex exists
+// fn regex(input: &String) -> (u32,u32) {
+//     let re_first = Regex::new(r"one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9").unwrap();
+//     let first = re_first.find(input).unwrap();
+//
+//     // FIXME: This doesn't work
+//     let re_last = Regex::new(r"^.*(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)").unwrap();
+//     let last = re_last.find(input).unwrap();
+//
+//     let mut first_num = 0;
+//     match first.as_str() {
+//         "one"|"1" => first_num = 1,
+//         "two"|"2" => first_num = 2,
+//         "three"|"3" => first_num = 3,
+//         "four"|"4" => first_num = 4,
+//         "five"|"5" => first_num = 5,
+//         "six"|"6" => first_num = 6,
+//         "seven"|"7" => first_num = 7,
+//         "eight"|"8" => first_num = 8,
+//         "nine"|"9" => first_num = 9,
+//         _ => panic!("invalid match"),
+//     }
+//     let mut last_num = 0;
+//     match last.as_str() {
+//         "one"|"1" => last_num = 1,
+//         "two"|"2" => last_num = 2,
+//         "three"|"3" => last_num = 3,
+//         "four"|"4" => last_num = 4,
+//         "five"|"5" => last_num = 5,
+//         "six"|"6" => last_num = 6,
+//         "seven"|"7" => last_num = 7,
+//         "eight"|"8" => last_num = 8,
+//         "nine"|"9" => last_num = 9,
+//         _ => panic!("invalid match"),
+//     }
+//
+//     (first_num,last_num)
+// }
+
+fn collect(input: &String) -> (u32,u32) {
+    let mut first:(usize,u32) = (usize::MAX,0);
+    let mut last:(usize,u32) = (0,0);
+
+    for (i,d) in DIGITS.iter().enumerate() {
+        if let Some(j) = input.find(d) {
+            if j < first.0 {
+                first.0 = j;
+                first.1 = i as u32;
+            }
+        }
+        if let Some(j) = input.rfind(d) {
+            let j = j+d.len()-1;
+            if j > last.0 {
+                last.0 = j;
+                last.1 = i as u32;
+            }
+        }
+    }
+    for (i,c) in input.chars().enumerate() {
+        if let Some(c) = c.to_digit(10) {
+            if i < first.0 {
+                first.0 = i;
+                first.1 = c;
+            }
+            // need to check for equal because 0 is initial
+            if i >= last.0 {
+                last.0 = i;
+                last.1 = c;
+            }
+        }
+    }
+    (first.1,last.1)
+}
+
 fn replace(input: &mut String) -> Result<()> {
-    const DIGITS:[&str;10] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     let mut is:[(Option<usize>,Option<usize>);DIGITS.len()] = [(None,None);DIGITS.len()];
 
     for (i,d) in DIGITS.iter().enumerate() {
@@ -134,16 +247,40 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works_b() {
+    fn replace_non_overlap() {
         let mut x = "oneeight".to_string();
         replace(&mut x).unwrap();
         assert_eq!(x, "18");
     }
 
     #[test]
-    fn it_works_a() {
+    fn replace_overlap() {
         let mut x = "oneight".to_string();
         replace(&mut x).unwrap();
         assert_eq!(x, "18");
+    }
+
+    #[test]
+    fn collect_non_overlap() {
+        let x = "oneeight".to_string();
+        let (first,last) = collect(&x);
+        assert_eq!(first, 1);
+        assert_eq!(last, 8);
+    }
+
+    #[test]
+    fn collect_overlap() {
+        let x = "oneight".to_string();
+        let (first,last) = collect(&x);
+        assert_eq!(first, 1);
+        assert_eq!(last, 8);
+    }
+
+    #[test]
+    fn collect_single_digit() {
+        let x = "4fefw".to_string();
+        let (first,last) = collect(&x);
+        assert_eq!(first, 4);
+        assert_eq!(last, 4);
     }
 }
